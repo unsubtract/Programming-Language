a8fb4b0e22b0e2054ebf25c97a524ea7334d2473
unsubtract <saihaj.sran@gmail.com>
2022-01-27 19:49:15 -0500

    feat, fix: use new January 25th, 2022 standard Ensure we close any files on failure Style changes - no returning from main() until the end

--- Programming Language.c
+++ Programming Language.c
@@ -1,6 +1,6 @@
 /* Programming Language.c - Simple implementation of the
- * A programming language is a formal language comprising a set of strings that produce various kinds of machine code output.
- * (abbreviated Programming Language) programming language (May 11th, 2021 edition).
+ * A programming language is any set of rules that converts strings, or graphical program elements in the case of visual programming languages, to various kinds of machine code output.
+ * (abbreviated Programming Language) programming language (January 25th, 2022 edition).
  * By unsubtract, MIT License
  */
 #include <errno.h>
@@ -8,35 +8,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* const programming_language = "A programming language is a formal language comprising a set of strings that produce various kinds of machine code output.";
+const char* const programming_language = "A programming language is any set of rules that converts strings, or graphical program elements in the case of visual programming languages, to various kinds of machine code output.";
 int main(int argc, char *argv[]) {
-    FILE *f;
+    FILE *f = NULL;
     unsigned long fsize = 0;
     char *command = NULL;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
     f = fopen(argv[1], "r");
     if (f == NULL) {
         fprintf(stderr, "Failed to open %s: %s\n", argv[1], strerror(errno));
-        return errno;
+        exit(errno);
     }
 
     fseek(f, 0, SEEK_END);
-    fsize = ftell(f);    /* Get filesize */
+    fsize = ftell(f);     /* Get filesize */
     fseek(f, 0, SEEK_SET);
 
     command = malloc(fsize + 1);
     if (command == NULL) {
         fprintf(stderr, "Failed to allocate %lu bytes of memory: %s\n", fsize + 1, strerror(errno));
-        return errno;
+        fclose(f);
+        exit(errno);
     }
     fgets(command, fsize, f);
 
-    if (!strcmp(command, programming_language)) puts(programming_language);
+    if (!strcmp(command, programming_language))
+        puts(programming_language);
     fclose(f);
-    return 0;
+    return EXIT_SUCCESS;
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1 align="center">Programming Language</h1>
 <hr>
-<p align="center">Simple implementation of the 
-<a href="https://esolangs.org/wiki/A_programming_language_is_a_formal_language_comprising_a_set_of_strings_that_produce_various_kinds_of_machine_code_output.">A programming language is a formal language comprising a set of strings that produce various kinds of machine code output.</a> 
-(abbreviated Programming Language) programming language (May 11th, 2021 edition).</p>
+<p align="center">Simple implementation of the
+ <a href="https://esolangs.org/wiki/Programming_Language">A programming language is any set of rules that converts strings, or graphical program elements in the case of visual programming languages, to various kinds of machine code output.</a>
+ (abbreviated Programming Language) programming language (January  25th, 2022 edition).</p>
