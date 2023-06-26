bd64754146b185237f0795df003dd2477a13cdf5
unsubtract <saihaj.sran@gmail.com>
2023-06-26 18:14:06 -0400

    treat files as binary, fixes about 1000000 bugs

--- Programming Language.c
+++ Programming Language.c
@@ -1,52 +1,55 @@
 /* Programming Language.c - Simple implementation of the
  * A programming language is a system of notation for writing
  * computer programs. (abbreviated Programming Language) programming language
- * (October 5, 2022 edition).
- * By unsubtract, MIT License
+ * (October 5, 2022 edition). By unsubtract, MIT License.
  */
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define BUFFER_SIZE strlen(programming_language) + 1 /* Adds 1 for null char */
-static const char* const programming_language = \
-"A programming language is a system of notation for writing \
+#define PROGRAMMING_LANGUAGE_LENGTH 77 /* exclude null char */
+static const char programming_language[78] =
+    "A programming language is a system of notation for writing \
 computer programs.";
 
 int main(int argc, char *argv[]) {
     FILE *f = NULL;
-    unsigned long fsize = 0;
-    char *command = NULL;
+    unsigned long fsize = 0, i = 0;
+    char input[PROGRAMMING_LANGUAGE_LENGTH];
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    f = fopen(argv[1], "r");
+    f = fopen(argv[1], "rb");
     if (f == NULL) {
         perror(argv[0]);
         exit(errno);
     }
 
-    /* first test if filesizes are equal before comparing strings */
-    fseek(f, 0, SEEK_END);/**/
-    fsize = ftell(f);     /* Get filesize */
-    fseek(f, 0, SEEK_SET);/**/
+    /* first test if filesizes are equal */
+    fseek(f, 0, SEEK_END);
+    fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
 
-    if (fsize != BUFFER_SIZE) {
+    if (fsize != PROGRAMMING_LANGUAGE_LENGTH) {
         fclose(f);
         exit(EXIT_SUCCESS);
     }
 
-    /* check if strings are equal */
-    command = malloc(BUFFER_SIZE);
-    fgets(command, BUFFER_SIZE, f);
-    command[strcspn(command, "\n")] = 0; /* strip newline */
+    /* check file contents for differences */
+    fread(input, 1, PROGRAMMING_LANGUAGE_LENGTH, f);
 
-    if (!strcmp(command, programming_language))
-        puts(programming_language);
+    /* i != PROGRAMMING_LANGUAGE_LENGTH if a difference is found */
+    while (i < PROGRAMMING_LANGUAGE_LENGTH &&
+           input[i] == programming_language[i])
+        ++i;
+
+    if (i == PROGRAMMING_LANGUAGE_LENGTH)
+        /* deliberately not EOL terminated, treat output as binary */
+        fwrite(programming_language, 1, PROGRAMMING_LANGUAGE_LENGTH, stdout);
+
     fclose(f);
     return EXIT_SUCCESS;
 }
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-<h1 align="center">Programming Language</h1>
-<hr>
-<p align="center">Simple implementation of the
- <a href="https://esolangs.org/wiki/Programming_Language">A programming language is a system of notation for writing computer programs.</a>
- (abbreviated Programming Language) programming language (October 5th, 2022 edition).</p>
+# Programming Language
 
- Run `make test` for a demonstration.
+---
+
+Simple implementation of the
+[A programming language is a system of notation for writing computer programs.](https://esolangs.org/wiki/Programming_Language)
+(abbreviated Programming Language) programming language (October 5th, 2022 edition).
+
+Program files in this implementation are treated as binary, and cannot contain any EOL characters or other trailing bytes (LF, CR, NUL, etc).
+That is to say, you have to remove any newline if your text editor allows you to.
+Because the output must always be the same as the input, the output is also deliberately not EOL terminated.
+
+Run `make test` for a demonstration.
