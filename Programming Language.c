e21ce8cceb99b073099e8b5e0481bf650c6e9324
unsubtract <saihaj.sran@gmail.com>
2023-06-26 19:55:12 -0400

    character array != string

--- Programming Language.c
+++ Programming Language.c
@@ -7,15 +7,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PROGRAMMING_LANGUAGE_LENGTH 77 /* exclude null char */
-static const char programming_language[78] =
+/* NOT NULL TERMINATED - should be treated as binary and not text */
+static const char programming_language[77] =
     "A programming language is a system of notation for writing \
 computer programs.";
 
 int main(int argc, char *argv[]) {
     FILE *f = NULL;
     unsigned long fsize = 0, i = 0;
-    char input[PROGRAMMING_LANGUAGE_LENGTH];
+    char input[sizeof(programming_language)];
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
@@ -33,22 +33,22 @@ int main(int argc, char *argv[]) {
     fsize = ftell(f);
     fseek(f, 0, SEEK_SET);
 
-    if (fsize != PROGRAMMING_LANGUAGE_LENGTH) {
+    if (fsize != sizeof(programming_language)) {
         fclose(f);
         exit(EXIT_SUCCESS);
     }
 
     /* check file contents for differences */
-    fread(input, 1, PROGRAMMING_LANGUAGE_LENGTH, f);
+    fread(input, 1, sizeof(programming_language), f);
 
-    /* i != PROGRAMMING_LANGUAGE_LENGTH if a difference is found */
-    while (i < PROGRAMMING_LANGUAGE_LENGTH &&
+    /* i != sizeof(programming_language) if a difference is found */
+    while (i < sizeof(programming_language) &&
            input[i] == programming_language[i])
         ++i;
 
-    if (i == PROGRAMMING_LANGUAGE_LENGTH)
+    if (i == sizeof(programming_language))
         /* deliberately not EOL terminated, treat output as binary */
-        fwrite(programming_language, 1, PROGRAMMING_LANGUAGE_LENGTH, stdout);
+        fwrite(programming_language, 1, sizeof(programming_language), stdout);
 
     fclose(f);
     return EXIT_SUCCESS;
