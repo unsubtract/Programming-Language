84fec1ae5170e1f376f6fcc205b2a7f0287e5425
unsubtract <saihaj.sran@gmail.com>
2023-06-27 23:30:27 -0400

    use fread return value to check file size

--- Programming Language.c
+++ Programming Language.c
@@ -7,48 +7,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* NOT NULL TERMINATED - should be treated as binary and not text */
-static const char programming_language[77] =
-    "A programming language is a system of notation for writing \
-computer programs.";
+/* NOT NULL TERMINATED - treat as binary and not a C string */
+static const char program[77] =
+    "A programming language is a system of notation for \
+writing computer programs.";
 
 int main(int argc, char *argv[]) {
     FILE *f = NULL;
-    unsigned long fsize = 0, i = 0;
-    char input[sizeof(programming_language)];
+    unsigned int i;
+    char input[sizeof(program) + 1];
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    f = fopen(argv[1], "rb");
-    if (f == NULL) {
+    if ((f = fopen(argv[1], "rb")) == NULL) {
         perror(argv[0]);
-        exit(errno);
+        exit(EXIT_FAILURE);
     }
 
-    /* first test if filesizes are equal */
-    fseek(f, 0, SEEK_END);
-    fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);
-
-    if (fsize != sizeof(programming_language)) {
+    /* read file and check filesize
+     * read 1 extra byte to detect if file is too big */
+    if (fread(input, 1, sizeof(program) + 1, f) != sizeof(program)) {
         fclose(f);
         exit(EXIT_SUCCESS);
     }
 
-    /* check file contents for differences */
-    fread(input, 1, sizeof(programming_language), f);
+    /* i stops short of sizeof(program) if a difference is found */
+    for (i = 0; i < sizeof(program) && input[i] == program[i]; ++i);
 
-    /* i != sizeof(programming_language) if a difference is found */
-    while (i < sizeof(programming_language) &&
-           input[i] == programming_language[i])
-        ++i;
-
-    if (i == sizeof(programming_language))
-        /* deliberately not EOL terminated, treat output as binary */
-        fwrite(programming_language, 1, sizeof(programming_language), stdout);
+    /* output is deliberately not EOL terminated and treated as binary */
+    if (i == sizeof(program)) fwrite(program, 1, sizeof(program), stdout);
 
     fclose(f);
     return EXIT_SUCCESS;
